76dff821802bc4d1083634aaabc10793b14a6c4d
AlexeyOtr <qsocket@yandex.ru>
2019-11-10 21:14:00 +0300

    add query database

--- src/medunica/DataBase.java
+++ src/medunica/DataBase.java
@@ -1,8 +1,14 @@
 package medunica;
 
+import java.awt.Component;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
+import javax.swing.JOptionPane;
+
+// 1. Конфиг для настройки подключения к БД
 
 public class DataBase {
 
@@ -17,7 +23,10 @@ public class DataBase {
             } catch (SQLException ex) {
                 System.err.println("SQLException: " + ex.getMessage());
                 System.err.println("SQLState: " + ex.getSQLState());
-                System.err.println("VendorError: " + ex.getErrorCode());         
+                System.err.println("VendorError: " + ex.getErrorCode());
+                Component frame = null;
+                JOptionPane.showMessageDialog(frame,"Ошибка подключения к БД","ОШИБКА",JOptionPane.ERROR_MESSAGE); 
+                System.exit(0);
             }
         }
     }
@@ -29,8 +38,24 @@ public class DataBase {
             return true;
             } catch (Exception ex) {
                 System.err.println("Ошибка загрузки драйвера БД " + ex);
+                Component frame = null;
+                JOptionPane.showMessageDialog(frame,"Ошибка загрузки драйвера БД","ОШИБКА",JOptionPane.ERROR_MESSAGE);
+                System.exit(0);
                 return false;
             }
     }
     
+    // Отправка запроса к БД, возвращает результат запроса
+    public ResultSet sendRequest(String query){
+            try {
+                Statement stmt = conn.createStatement();
+                return stmt.executeQuery(query);
+            } catch (SQLException ex) {
+                System.err.println("Ошибка запроса к БД");
+                Component frame = null;
+                JOptionPane.showMessageDialog(frame,"Ошибка отправки запроса","ОШИБКА",JOptionPane.ERROR_MESSAGE);
+                System.exit(0);
+                return null;
+            }
+    }
 }
